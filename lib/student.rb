9045028f2a2538becbb39648c3fa23a18b131007
Sumer   Gaikwad <sg.comparch@gmail.com>
2021-01-06 19:46:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,11 +15,7 @@ class Student
   
     def self.create_table
     sql =  <<-SQL 
-      CREATE TABLE IF NOT EXISTS students (
-        id INTEGER PRIMARY KEY, 
-        name TEXT, 
-        grade INTEGER
-        )
+      
         SQL
     DB[:conn].execute(sql) 
   end
